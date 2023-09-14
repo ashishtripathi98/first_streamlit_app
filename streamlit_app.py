3c9171b182aeefdfdf80349dec771f89b97b4b01
ashishtripathi98 <144660024+ashishtripathi98@users.noreply.github.com>
2023-09-14 15:15:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,7 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruity_choice)
     streamlit.dataframe(back_from_function)
-  except URLError as e:
+   
+except URLError as e:
    streamlit.error()
    

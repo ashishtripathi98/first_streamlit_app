44e6fb12fb3a37330a2300ce21d713ef3a946ca4
ashishtripathi98 <144660024+ashishtripathi98@users.noreply.github.com>
2023-09-15 09:59:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit Load List Contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 
 #new section to display fruityvice API
